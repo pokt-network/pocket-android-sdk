22836139cad3d9346f806e556cf54955b5465ff7
Luis de Leon <luyzdeleon@gmail.com>
2018-08-22 13:40:30 -0400

    Implemented Codable

--- app/src/main/java/network/pokt/pocketsdk/models/Transaction.java
+++ app/src/main/java/network/pokt/pocketsdk/models/Transaction.java
@@ -1,50 +1,53 @@
 package network.pokt.pocketsdk.models;
 
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.util.Map;
+import network.pokt.pocketsdk.interfaces.Codable;
 
-public class Transaction {
+public class Transaction implements Codable {
 
-    protected class Request {
+    protected class Request extends JSONObject {
 
         @NotNull public String network;
         @NotNull public String subnetwork;
         @NotNull public String serializedTransaction;
-        @NotNull public Map<@NotNull String, Object> transactionMetadata;
+        @NotNull public JSONObject transactionMetadata;
 
-        public Request(@NotNull String network, @NotNull String subnetwork, @NotNull String serializedTransaction, Map<@NotNull String, Object> transactionMetadata) {
+        public Request(@NotNull String network, @NotNull String subnetwork, @NotNull String serializedTransaction, @NotNull JSONObject transactionMetadata) throws JSONException {
+            super();
             this.network = network;
             this.subnetwork = subnetwork;
             this.serializedTransaction = serializedTransaction;
             this.transactionMetadata = transactionMetadata;
+            this.put("network", network);
+            this.put("subnetwork", subnetwork);
+            this.put("serialized_tx", serializedTransaction);
+            this.put("tx_metadata", transactionMetadata);
         }
     }
 
-    protected class Response {
+    protected class Response extends JSONObject {
         public String hash;
-        @NotNull public Map<@NotNull String, Object> metadata;
-        @NotNull public boolean error;
+        public JSONObject metadata;
+        public boolean error;
         public String errorMsg;
 
-        public Response(String hash, @NotNull Map<@NotNull String, Object> metadata, @NotNull boolean error, String errorMsg) {
-            this.hash = hash;
-            this.metadata = metadata;
-            this.error = error;
-            this.errorMsg = errorMsg;
+        public Response(String jsonString) throws JSONException {
+            super(jsonString);
+            this.hash = this.optString("hash");
+            this.metadata = this.optJSONObject("metadata");
+            this.error = this.getBoolean("error");
+            this.errorMsg = this.optString("error_msg");
         }
     }
 
     private Request request;
     private Response response;
 
-    public Transaction(@NotNull String network, @NotNull String subnetwork, @NotNull String serializedTransaction, @NotNull Map<@NotNull String, Object> transactionMetadata) {
+    public Transaction(@NotNull String network, @NotNull String subnetwork, @NotNull String serializedTransaction, @NotNull JSONObject transactionMetadata) throws JSONException {
         this.request = new Request(network, subnetwork, serializedTransaction, transactionMetadata);
-    }
-
-    public Transaction(@NotNull String network, @NotNull String subnetwork, @NotNull String serializedTransaction, @NotNull Map<@NotNull String, Object> transactionMetadata, String hash, @NotNull Map<@NotNull String, Object> metadata, @NotNull boolean error, String errorMsg) {
-        this.request = new Request(network, subnetwork, serializedTransaction, transactionMetadata);
-        this.response = new Response(hash, metadata, error, errorMsg);
     }
 
     public boolean isSent() {
@@ -55,32 +58,50 @@ public class Transaction {
         return this.request.network;
     }
 
+    @SuppressWarnings("unused")
     public String getSubnetwork() {
         return this.request.subnetwork;
     }
 
+    @SuppressWarnings("unused")
     public String getSerializedTransaction() {
         return this.request.serializedTransaction;
     }
 
-    public Map<@NotNull String, Object> getTransactionMetadata() {
+    @SuppressWarnings("unused")
+    public JSONObject getTransactionMetadata() {
         return this.request.transactionMetadata;
     }
 
+    @SuppressWarnings("unused")
     public String getHash() {
         return isSent() ? this.response.hash : null;
     }
 
-    public  Map<@NotNull String, Object> getMetadata() {
+    @SuppressWarnings("unused")
+    public  JSONObject getMetadata() {
         return isSent() ? this.response.metadata : null;
     }
 
+    @SuppressWarnings("unused")
     public boolean isError() {
+        //noinspection ConstantConditions
         return isSent() ? this.response.error : null;
     }
 
+    @SuppressWarnings("unused")
     public String getErrorMsg() {
         return isSent() ? this.response.errorMsg : null;
+    }
+
+    @Override
+    public String toJSONRequestString() {
+        return this.request.toString();
+    }
+
+    @Override
+    public void fromJSONResponseString(String json) throws JSONException {
+        this.response = new Response(json);
     }
 
 }
