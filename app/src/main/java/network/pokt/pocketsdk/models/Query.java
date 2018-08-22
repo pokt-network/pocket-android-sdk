e1beb5d54d627c84ab013074ed10963fefd84c78
Luis de Leon <luyzdeleon@gmail.com>
2018-08-22 13:40:30 -0400

    Implemented Codable

--- app/src/main/java/network/pokt/pocketsdk/models/Query.java
+++ app/src/main/java/network/pokt/pocketsdk/models/Query.java
@@ -1,48 +1,52 @@
 package network.pokt.pocketsdk.models;
 
-import java.util.Map;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class Query {
+import network.pokt.pocketsdk.interfaces.Codable;
 
-    protected class Request {
+public class Query implements Codable {
+
+    protected class Request extends JSONObject {
         @NotNull public String network;
         @NotNull public String subnetwork;
-        @NotNull public Map<@NotNull String, Object> data;
-        @NotNull public Map<@NotNull String, Object> decoder;
+        @NotNull public JSONObject data;
+        @NotNull public JSONObject decoder;
 
-        public Request(@NotNull String network, @NotNull String subnetwork, @NotNull Map<@NotNull String, Object> data, @NotNull Map<@NotNull String, Object> decoder) {
+        public Request(@NotNull String network, @NotNull String subnetwork, @NotNull JSONObject data, @NotNull JSONObject decoder) throws JSONException {
+            super();
             this.network = network;
             this.subnetwork = subnetwork;
             this.data = data;
             this.decoder = decoder;
+            this.put("network", network);
+            this.put("subnetwork", subnetwork);
+            this.put("query", data);
+            this.put("decoder", decoder);
         }
     }
 
-    protected class Response {
-        public Map<@NotNull String, Object> result;
-        @NotNull public boolean decoded;
-        @NotNull public boolean error;
+    protected class Response extends JSONObject {
+        public JSONObject result;
+        public boolean decoded;
+        public boolean error;
         public String errorMsg;
 
-        public Response(Map<@NotNull String, Object> result, @NotNull boolean decoded, @NotNull boolean error, String errorMsg) {
-            this.result = result;
-            this.decoded = decoded;
-            this.error = error;
-            this.errorMsg = errorMsg;
+        public Response(String jsonString) throws JSONException {
+            super(jsonString);
+            this.result = this.optJSONObject("result");
+            this.decoded = this.getBoolean("decoded");
+            this.error = this.getBoolean("error");
+            this.errorMsg = this.optString("error_msg");
         }
     }
 
     @NotNull protected Request request;
     protected Response response;
 
-    public Query(@NotNull String network, @NotNull String subnetwork, @NotNull Map<@NotNull String, Object> data, @NotNull Map<@NotNull String, Object> decoder) {
+    public Query(@NotNull String network, @NotNull String subnetwork, @NotNull JSONObject data, @NotNull JSONObject decoder) throws JSONException {
         this.request = new Request(network, subnetwork, data, decoder);
-    }
-
-    public Query(@NotNull String network, @NotNull String subnetwork, @NotNull Map<@NotNull String, Object> data, @NotNull Map<@NotNull String, Object> decoder, Map<@NotNull String, Object> result, @NotNull boolean decoded, @NotNull boolean error, String errorMsg) {
-        this.request = new Request(network, subnetwork, data, decoder);
-        this.response = new Response(result, decoded, error, errorMsg);
     }
 
     public boolean isExecuted() {
@@ -53,31 +57,49 @@ public class Query {
         return this.request.network;
     }
 
+    @SuppressWarnings("unused")
     public String getSubnetwork() {
         return this.request.subnetwork;
     }
 
-    public Map<String, Object> getData() {
+    public JSONObject getData() {
         return this.request.data;
     }
 
-    public Map<String, Object> getDecoder() {
+    @SuppressWarnings("unused")
+    public JSONObject getDecoder() {
         return this.request.decoder;
     }
 
-    public Map<String, Object> getResult() {
+    @SuppressWarnings("unused")
+    public JSONObject getResult() {
         return isExecuted() ? this.response.result : null;
     }
 
+    @SuppressWarnings("unused")
     public boolean isDecoded() {
+        //noinspection ConstantConditions
         return isExecuted() ? this.response.decoded : null;
     }
 
+    @SuppressWarnings("unused")
     public boolean isError() {
+        //noinspection ConstantConditions
         return isExecuted() ? this.response.error : null;
     }
 
+    @SuppressWarnings("unused")
     public String getErrorMsg() {
         return isExecuted() ? this.response.errorMsg : null;
+    }
+
+    @Override
+    public String toJSONRequestString() {
+        return this.request.toString();
+    }
+
+    @Override
+    public void fromJSONResponseString(String json) throws JSONException {
+        this.response = new Response(json);
     }
 }
