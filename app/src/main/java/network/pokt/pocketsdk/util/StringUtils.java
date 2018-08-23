ba5b1c8a1398c331836edb2c4dd1cf7d0c65c7ee
Luis de Leon <luyzdeleon@gmail.com>
2018-08-23 15:30:22 -0400

    Added StringUtils

---
+++ app/src/main/java/network/pokt/pocketsdk/util/StringUtils.java
@@ -0,0 +1,9 @@
+package network.pokt.pocketsdk.util;
+
+public class StringUtils {
+
+    public static boolean isValidString(String string) {
+        return string != null && !string.isEmpty();
+    }
+
+}
